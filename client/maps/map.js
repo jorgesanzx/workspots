836aed4ed38e71a474004b59e0e9286beb2cfb67
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-10-11 14:05:46 +0200

    changed current position icon

--- client/maps/map.js
+++ client/maps/map.js
@@ -22,9 +22,17 @@ Template.map.onCreated(function() {
   GoogleMaps.load({ v: '3'});
 
   GoogleMaps.ready('testMap', function(map) {
+    var locationIcon = {
+      url: '/images/position.png',
+      size: new google.maps.Size(22, 22),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(11, 11)
+    };
+
     var marker = new google.maps.Marker({
       map: map.instance,
-      position: map.options.center
+      position: map.options.center,
+      icon: locationIcon
     });
 
     Spots.find().fetch().forEach(function(spot) {

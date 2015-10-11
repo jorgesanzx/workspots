3768db83fd9c14ced8f32f1eb4cd367a200ec70c
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-10-11 19:51:01 +0200

    currently refactoring map spots markers with infoWindow

--- client/maps/map.js
+++ client/maps/map.js
@@ -1,63 +1,77 @@
-var globalMap;
-var markers = [];
+var SpotMarker = function(map, spot) {
+  var self = this;
+  this.spot = spot;
+  this.map = map;
 
-function showMarkers(map, spots) {
-  if(!map)
-    return false;
-
-  var infowindow;
-  var infoWindowOpen = null;
-  var marker;
-
-  //remove markers
-  if(markers){
-    markers.forEach( function(marker) {
-      marker.setMap(null);
+  this.buildMarker = function() {
+    return new google.maps.Marker({
+      map: this.map.instance,
+      position: {lat: this.spot.position.latitude, lng: this.spot.position.longitude},
+      title: this.spot.name
     });
-  }
-  
-  function showSpot(spot) {
-    marker = new google.maps.Marker({
-      map: map.instance,
-      position: {lat: spot.position.latitude, lng: spot.position.longitude},
-      title: spot.name
-    });
-
-    marker.addListener('click', function() {
-      //if a infowindow is opened -> close
-      if (infoWindowOpen) infoWindowOpen.close();
-
-      var infowindowContent = `<h2><a href="geo:${spot.position.latitude},${spot.position.longitude}">${spot.name}</a></h2>
-                              <p><strong>Wi-Fi Quality:</strong> ${spot.wiFiQuality}
-                              <p><strong>Power Available:</strong> ${spot.powerAvailable ? "Yes" : "No"}`
-      //add comments
-      if(spot.comments && spot.comments.length > 0) {
-        infowindowContent +=  `<p><strong>Comments:</strong></p>
-                              <ul>`;
-        spot.comments.forEach( function(comment){
-          infowindowContent += `<li>${comment}</li>`;
-        });
-
-        infowindowContent += `</ul>`;
-      }
-      
-      infowindow = new google.maps.InfoWindow({
-        content: infowindowContent
-      });
-
-      infowindow.open(map.instance, this);  
-      infoWindowOpen = infowindow;
-    });
-
-    markers.push(marker);
+    marker.addListener('click', this.clicked);
   };
-  
-  markers = [];
 
-  console.log(spots);
-  spots.forEach(showSpot);
+  this.buildWindow = function() {
+    var spot = this.spot;
+    var infowindowContent = `<h2><a href="geo:${spot.position.latitude},${spot.position.longitude}">${spot.name}</a></h2>
+                            <p><strong>Wi-Fi Quality:</strong> ${spot.wiFiQuality}
+                            <p><strong>Power Available:</strong> ${spot.powerAvailable ? "Yes" : "No"}`
+    //add comments
+    if(spot.comments && spot.comments.length > 0) {
+      infowindowContent +=  `<p><strong>Comments:</strong></p>
+                            <ul>`;
+      spot.comments.forEach(function(comment) {
+        infowindowContent += `<li>${comment}</li>`;
+      });
+      infowindowContent += `</ul>`;
+    }
+
+    return new google.maps.InfoWindow({
+      content: infowindowContent
+    });
+  };
+
+  this.click = function() {
+    console.debug('clicked on spotmarker ', this);
+  };
+
+  this.delete = function() {
+    this.marker.setMap(null);
+  };
+
+  this.toString = function() {
+    return `[SpotMarker ${this.spot.name}]`;
+  }
+
+  this.window = this.buildWindow();
+  this.marker = this.buildMarker();
+  this.marker.spotmarker = this;
+  this.marker.addListener('click', function() {
+    var sm = this.spotmarker;
+    FlowRouter.go('/?spot=' + this.spotmarker.spot._id);
+  });
 }
 
+var spotmarkers = {
+  map: null,
+  currentSpotMarker: null,
+  spotMarkers: [],
+
+  add: function(spot) {
+    var spotMarker = new SpotMarker(spotmarkers.map, spot);
+    spotMarker.click = function() {
+      if (spotmarkers.currentSpotMarker) {
+        spotmarkers.currentSpotMarker.window.close();
+      }
+    }
+    spotmarkers.spotMarkers.push(spotMarker);
+  },
+
+  addList: function(list) {
+    this.spotMarkers = list.map(this.add);
+  }
+};
 
 Template.map.events({
   'click .googleMapContainer': function(event){
@@ -67,7 +81,7 @@ Template.map.events({
       FlowRouter.go('/');
     }
   }
-})
+});
 
 Template.map.helpers({
   geolocationError: function() {
@@ -90,18 +104,14 @@ Template.map.helpers({
 
   getFilters: function() {
     var filters = Session.get('filters');
-    
+
     if(filters && filters.wifi && filters.power){
       var query = {
         wiFiQuality: {$gte: Number(filters.wifi)}
       };
-
       if(filters.power && filters.power == "yes"){
         query.powerAvailable = true;
       }
-
-      console.log(query);
-
       var spots = Spots.find( query ).fetch();
 
       showMarkers(globalMap, spots);
@@ -110,27 +120,28 @@ Template.map.helpers({
 
 });
 
+function showCurrentUser(map) {
+  var locationIcon = {
+    url: '/images/position.png',
+    size: new google.maps.Size(22, 22),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(11, 11)
+  };
+
+  var userMarker = new google.maps.Marker({
+    map: map.instance,
+    position: map.options.center,
+    icon: locationIcon
+  });
+};
+
 Template.map.onCreated(function() {
   GoogleMaps.load({ v: '3'});
 
-  GoogleMaps.ready('testMap', function(map) {
-
+  GoogleMaps.ready('googleMap', function(map) {
     globalMap = map;
-
-    var locationIcon = {
-      url: '/images/position.png',
-      size: new google.maps.Size(22, 22),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(11, 11)
-    };
-
-    var userMarker = new google.maps.Marker({
-      map: map.instance,
-      position: map.options.center,
-      icon: locationIcon
-    });
-
-    var spot = Spots.find().fetch();
-    showMarkers(map, Spots.find().fetch());
+    showCurrentUser(map);
+    spotmarkers.map = map;
+    spotmarkers.addList(Spots.find().fetch());
   });
 });

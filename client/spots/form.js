cf3a9737413a8bc73e52b8326898423bc61da53e
Simon <oleurud@gmail.com>
2015-10-11 12:58:20 +0200

    spot collection and submit event

---
+++ client/spots/form.js
@@ -0,0 +1,28 @@
+Template.spotForm.events({
+	"submit .spot-form": function(event) {
+		event.preventDefault();
+
+    if(!Geolocation.error()) {
+      var latLng = Geolocation.latLng();
+
+  		var spot = {
+  			name: $('#name').val(),
+  			wiFiQuality: $('#wiFiQuality').val(),
+  			powerAvailable: $('#powerAvailable').is(':checked'),
+  			comments: [$('#comment').val()],
+        position: {
+          latitude: latLng.lat,
+          longitude: latLng.lng
+        }
+  		}
+
+  		//TODO: validation
+
+  		//insert
+  		Spots.insert(spot);
+
+      //go home
+      FlowRouter.go('/');      
+    }
+	} 
+});

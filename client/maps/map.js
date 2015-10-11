20435e0657c60802bdfc1d3d9b40c49d2af83bd2
Simon <oleurud@gmail.com>
2015-10-11 14:08:01 +0200

    map spots infowindow

--- client/maps/map.js
+++ client/maps/map.js
@@ -1,3 +1,13 @@
+Template.map.events({
+  'click .googleMapContainer': function(event){
+    if(FlowRouter.current().path == '/spots/new'){
+      event.preventDefault();
+      //go home
+      FlowRouter.go('/');
+    }
+  }
+})
+
 Template.map.helpers({
   geolocationError: function() {
     var error = Geolocation.error();
@@ -22,16 +32,35 @@ Template.map.onCreated(function() {
   GoogleMaps.load({ v: '3'});
 
   GoogleMaps.ready('testMap', function(map) {
-    var marker = new google.maps.Marker({
+    var userMarker = new google.maps.Marker({
       map: map.instance,
       position: map.options.center
     });
 
+    var infowindow;
+    var infoWindowOpen = null;
+    var marker;
     Spots.find().fetch().forEach(function(spot) {
       marker = new google.maps.Marker({
         map: map.instance,
         position: {lat: spot.position.latitude, lng: spot.position.longitude},
         title: spot.name
+      });
+
+      marker.addListener('click', function() {
+        //if a infowindow is opened -> close
+        if (infoWindowOpen) infoWindowOpen.close();
+
+        var infowindowContent = '<h2>' + spot.name + '</h2>' +
+                                '<p><strong>Wi-Fi Quality:</strong> ' + spot.wiFiQuality +
+                                '<p><strong>Power Available:</strong> ' + spot.powerAvailable
+        
+        infowindow = new google.maps.InfoWindow({
+          content: infowindowContent
+        });
+
+        infowindow.open(map.instance, marker);
+        infoWindowOpen = infowindow;
       });
     });
   });
